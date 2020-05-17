4a052604b4bb25eaf8a75f68910f16a569ec6d7f
dantelyon <dightmisc@gmail.com>
2020-05-17 16:35:31 +0200

    included a miscellany of minor fp techniques.

--- fp_module.js
+++ fp_module.js
@@ -7,3 +7,27 @@ const userAgent = () => navigator.userAgent
 const storage = () => [!!window.localStorage, !!window.sessionStorage, !!window.indexedDB, localStorage.length, localStorage.key(0)]
 
 const fullscreen = () => document.fullscreenEnabled
+
+const deviceMemory = () => navigator.deviceMemory // limited availability
+
+const hardwareConcurrency = () => navigator.hardwareConcurrency
+
+const doNotTrack = () => navigator.doNotTrack == "1" || window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.msDoNotTrack == "1"
+
+const sessionHistory = () => window.history.length
+
+const cookieEnabled = () => navigator.cookieEnabled
+
+const webdriver = () => navigator.webdriver == null ? "not available" : navigator.webdriver
+
+function language() {
+	let lang = navigator.language || navigator.userLanguage || navigator.browserLanguage || navigator.systemLanguage || "not available"
+	return navigator.languages.includes(lang) ? navigator.languages : lang
+}
+
+
+
+
+
+
+
