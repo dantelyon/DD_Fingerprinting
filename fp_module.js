77fe8fceca6f64ca362e905a3559f8934490477c
dantelyon <dightmisc@gmail.com>
2020-05-22 16:28:24 +0200

    included the webgl function

--- fp_module.js
+++ fp_module.js
@@ -83,27 +83,57 @@ function renderer() {
 	}
 }
 
-function DOMRect(){
-        let elem = document.createElement('div');
-        let s = elem.style;
-        s.position = 'absolute';
-        s.left = '3.1px';
-        s.top = '2.1px';
-        s.zIndex = '-100';
-        s.visibility = 'hidden';
-        s.fontSize = '19.123px';
-        s.transformOrigin = '0.1px 0.2px 0.3px';
-        s.webkitTransformOrigin = '0.1px 0.2px 0.3px';
-        s.webkitTransform = 'scale(1.01123) matrix3d(0.251106, 0.0131141, 0, -0.000109893, -0.0380797, 0.349552, 0, 7.97469e-06, 0, 0, 1, 0, 575, 88, 0, 1)';
-        s.transform = 'scale(1.01123) matrix3d(0.251106, 0.0131141, 0, -0.000109893, -0.0380797, 0.349552, 0, 7.97469e-06, 0, 0, 1, 0, 575, 88, 0, 1)';
-        elem.innerHTML = '<h1>Sed ut perspiciatis unde</h1>dahlberg<b>dahl<i id="target">dahl</i></b>';
-        document.body.appendChild(elem);
-        let rect = document.getElementById('target').getClientRects()[0];
-        elem.remove();
-        return rect
+function domRect(){
+	let elem = document.createElement('div');
+	let s = elem.style;
+	s.position = 'absolute';
+	s.left = '3.1px';
+	s.top = '2.1px';
+	s.zIndex = '-100';
+	s.visibility = 'hidden';
+	s.fontSize = '19.123px';
+	s.transformOrigin = '0.1px 0.2px 0.3px';
+	s.webkitTransformOrigin = '0.1px 0.2px 0.3px';
+	s.webkitTransform = 'scale(1.01123) matrix3d(0.251106, 0.0131141, 0, -0.000109893, -0.0380797, 0.349552, 0, 7.97469e-06, 0, 0, 1, 0, 575, 88, 0, 1)';
+	s.transform = 'scale(1.01123) matrix3d(0.251106, 0.0131141, 0, -0.000109893, -0.0380797, 0.349552, 0, 7.97469e-06, 0, 0, 1, 0, 575, 88, 0, 1)';
+	document.body.appendChild(elem);
+	let rect = document.getElementById('target').getClientRects()[0];
+	elem.remove();
+	return rect
 }
 
-
+function webGL() {
+	let canvas = document.createElement('canvas')
+	let gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+	let vertexPosBuffer = gl.createBuffer()
+	let vertices = new Float32Array([-0.2, -0.9, 0, 0.4, -0.26, 0, 0, 0.732134444, 0])
+	let program = gl.createProgram()
+    let vshader = gl.createShader(gl.VERTEX_SHADER)
+    let fshader = gl.createShader(gl.FRAGMENT_SHADER)
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	gl.enable(gl.DEPTH_TEST)
+	gl.depthFunc(gl.LEQUAL)
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+    vertexPosBuffer.itemSize = 3
+    vertexPosBuffer.numItems = 3
+    gl.shaderSource(vshader, 'attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}')
+    gl.compileShader(vshader)
+    gl.shaderSource(fshader, 'precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}')
+    gl.compileShader(fshader)
+    gl.attachShader(program, vshader)
+    gl.attachShader(program, fshader)
+    gl.linkProgram(program)
+    gl.useProgram(program)
+    program.vertexPosAttrib = gl.getAttribLocation(program, 'attrVertex')
+    program.offsetUniform = gl.getUniformLocation(program, 'uniformOffset')
+    gl.enableVertexAttribArray(program.vertexPosArray)
+    gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0)
+    gl.uniform2f(program.offsetUniform, 1, 1)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
+    return gl.canvas.toDataURL()
+}
 
 
 
