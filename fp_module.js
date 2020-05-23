e6c9c0e134851a47b844027f275f56a589247bd7
dantelyon <dightmisc@gmail.com>
2020-05-23 12:25:20 +0200

    included canvas function

--- fp_module.js
+++ fp_module.js
@@ -108,33 +108,53 @@ function webGL() {
 	let vertexPosBuffer = gl.createBuffer()
 	let vertices = new Float32Array([-0.2, -0.9, 0, 0.4, -0.26, 0, 0, 0.732134444, 0])
 	let program = gl.createProgram()
-    let vshader = gl.createShader(gl.VERTEX_SHADER)
-    let fshader = gl.createShader(gl.FRAGMENT_SHADER)
+	let vshader = gl.createShader(gl.VERTEX_SHADER)
+	let fshader = gl.createShader(gl.FRAGMENT_SHADER)
 	gl.clearColor(0.0, 0.0, 0.0, 1.0)
 	gl.enable(gl.DEPTH_TEST)
 	gl.depthFunc(gl.LEQUAL)
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-    vertexPosBuffer.itemSize = 3
-    vertexPosBuffer.numItems = 3
-    gl.shaderSource(vshader, 'attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}')
-    gl.compileShader(vshader)
-    gl.shaderSource(fshader, 'precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}')
-    gl.compileShader(fshader)
-    gl.attachShader(program, vshader)
-    gl.attachShader(program, fshader)
-    gl.linkProgram(program)
-    gl.useProgram(program)
-    program.vertexPosAttrib = gl.getAttribLocation(program, 'attrVertex')
-    program.offsetUniform = gl.getUniformLocation(program, 'uniformOffset')
-    gl.enableVertexAttribArray(program.vertexPosArray)
-    gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0)
-    gl.uniform2f(program.offsetUniform, 1, 1)
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
-    return gl.canvas.toDataURL()
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+	vertexPosBuffer.itemSize = 3
+	vertexPosBuffer.numItems = 3
+	gl.shaderSource(vshader, 'attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}')
+	gl.compileShader(vshader)
+	gl.shaderSource(fshader, 'precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}')
+	gl.compileShader(fshader)
+	gl.attachShader(program, vshader)
+	gl.attachShader(program, fshader)
+	gl.linkProgram(program)
+	gl.useProgram(program)
+	program.vertexPosAttrib = gl.getAttribLocation(program, 'attrVertex')
+	program.offsetUniform = gl.getUniformLocation(program, 'uniformOffset')
+	gl.enableVertexAttribArray(program.vertexPosArray)
+	gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0)
+	gl.uniform2f(program.offsetUniform, 1, 1)
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
+	return gl.canvas.toDataURL()
 }
 
-
+function canvas(){
+	let canvas = document.createElement("canvas")
+	let context = canvas.getContext("2d")
+    canvas.width = 320
+    canvas.height = 200
+	context.font = "14px 'Arial'"
+	context.fillStyle = "#f60"
+	context.fillRect(20, 1, 62, 100)
+	context.fillStyle = "#069"
+	context.fillText("MNOP_dahlberg~1!2@", 2, 15);
+	context.fillStyle = "rgba(102, 204, 0, 0.7)"
+	context.fillText("MNOP_dahlberg~1!2@", 4, 45)
+	context.fillStyle = "rgb(255,0,255)"
+	context.fillRect(200, 1, 62, 100)
+	let data = canvas.toDataURL();
+	let hash = 0;
+	for (let i = 0; i < data.length; i++) {
+		hash = (((hash<<5) - hash) + data.charCodeAt(i));
+	}
+	return (hash&0xFFFFFFFF).toString(16);
+}
 
 
