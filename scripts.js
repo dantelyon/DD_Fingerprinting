c98634c14723a5011d866b787234b383fc2b4c38
dantelyon <dightmisc@gmail.com>
2020-05-26 11:05:13 +0200

    added some behavioural data points.

--- scripts.js
+++ scripts.js
@@ -11,5 +11,24 @@ techniques.forEach(t => {
 	document.getElementById("table").appendChild(tr)
 })
 
+window.addEventListener('resize', event => document.querySelector(".pixel-ratio").innerText = (window.devicePixelRatio * 100).toFixed(0)+"%"); //zoom level
 
+let keydownEvents = []
+document.addEventListener('keydown', event => {
+	console.log(`${event.code} (${event.keyCode}) at ${event.timeStamp}ms (after document creation).`)
+	keydownEvents.push(event.keyCode)
+	console.log(keydownEvents.length)
+});
 
+let mousedownEvents = []
+document.addEventListener("mousedown", event => {
+	console.log(`X:${event.clientX} Y:${event.clientY} at ${event.timeStamp}ms (after document creation).`)
+	mousedownEvents.push(event.keyCode)
+	console.log(mousedownEvents.length)
+})
+
+document.addEventListener('mousemove', event => {
+	document.getElementById("#screen-log").innerText = `
+    Screen X/Y: ${event.screenX}, ${event.screenY}
+    Client X/Y: ${event.clientX}, ${event.clientY}`;
+});
