308b63b935f2e0f159678a3df929261cd0b5c73d
dantelyon <dightmisc@gmail.com>
2020-09-16 13:00:57 +0200

    table design completed

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Techniques from './Techniques'
+import './Table.css';
 
 export default function Table() {
     return <table className="table">
+                  <colgroup>
+                  <col id="col1"></col>
+                  <col id="col2"></col>
+                  </colgroup>
         <tbody>
             {Techniques.map(t => <TableRow props={t}/>)}
         </tbody>
